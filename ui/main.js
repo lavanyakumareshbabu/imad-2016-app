82522b596da062cccf0c09b7c66817517aba8850
lavanyakumareshbabu <lavanyakb39@gmail.com>
2016-09-23 12:22:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,7 @@
 console.log('Loaded!');
 
 var element = document.getElementbyId('main-text');
-
-element.innerHTML = "java script text";
+function myfunction()
+{
+  element.innerHTML = "java script text";
+} 
