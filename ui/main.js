4af674563c55aa2bf41651c649b7260eaa1873da
lavanyakumareshbabu <lavanyakb39@gmail.com>
2016-09-24 07:44:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ var element = document.getElementById('main-text');
 
 element.innerHTML = "NEW VALUE";
 
+var img =document.getElementById('madi');
+
+img.onclick = function(){
+    
+    img.style.marginleft = '100px';
+    
+}
