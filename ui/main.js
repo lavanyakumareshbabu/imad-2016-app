a8dcf7c32bdd7797a77ebc2e26a56184ca48d06b
lavanyakumareshbabu <lavanyakb39@gmail.com>
2016-09-24 08:51:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,9 @@ var element = document.getElementById('main-text');
 
 element.innerHTML = "NEW VALUE";
 var img=document.getElementById("madi");
-var marginleft = 0;
-function moveright(){
-    marginleft=marginleft+2;
-    img.style.marginleft=marginleft+"px";
-}
-img.onclick=function(){
-    var interval=Setinterval(moveright,50);};
-   // img.style.marginLeft="100px";};
+
+var marginleft=0;
+
+function moveright(){ marginleft=marginleft+2; img.style.marginLeft=marginleft+"px";}
+
+img.onclick=function(){ var interval = setInterval(moveright,50);};
