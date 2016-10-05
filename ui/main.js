1b1113c84cbb4fe750716b2d26f988409961e1f9
lavanyakumareshbabu <lavanyakb39@gmail.com>
2016-10-05 08:54:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,4 +9,4 @@ var marginleft=0;
 
 function moveright(){ marginleft=marginleft+2; img.style.marginLeft=marginleft+"px";}
 
-img.onclick=function(){ var interval = setInterval(moveright,50);};
+img.onclick=function(){ var interval = setInterval(moveright,10);};
