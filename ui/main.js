32b998eab79c4bef184d55bdf18759dc5a3fc0da
lavanyakumareshbabu <lavanyakb39@gmail.com>
2016-10-18 20:30:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,14 @@ console.log('loaded');
 
 var img=document.getElementById('madi');
 
-img.onclick=function(){img.style.marginLeft="100px";};
+var marginleft=0;
+function moveright()
+{
+    marginleft=marginleft+1;
+    img.style.marginLeft=marginleft+'px';
+}
+img.onclick=function()
+{
+    var interval=setInterval(moveright,50);
+};
+    
