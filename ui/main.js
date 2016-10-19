00c7b82f0bcfddd575c680b50f053da4a30b601d
lavanyakumareshbabu <lavanyakb39@gmail.com>
2016-10-19 13:05:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,7 +33,7 @@ button.onclick=function()
     
     };
 };
-request.open('GET','http://lavanyakumareshbabu.imad-hasura-app.io/counter',true);
+request.open('GET','http://lavanyakumareshbabu.imad.hasura-app.io/counter',true);
 request.send(null);
 
 
