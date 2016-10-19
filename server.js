8e4f2b57f351a057b1407dd26cca15e9bd29f35b
lavanyakumareshbabu <lavanyakb39@gmail.com>
2016-10-19 10:30:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,19 +73,20 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
- 
-app.get('/:articlename', function (req, res) {
-    var articlename=req.params.articlename;
-  res.send(createtemplate(articles[articlename]));
-});
-
- app.get('/ui/style.css', function (req, res) {
+app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+ 
+app.get('/:articlename', function (req, res) {
+    var articlename=req.params.articlename;
+  res.send(createtemplate(articles[articlename]));
+});
+
+ 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
