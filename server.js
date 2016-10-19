d6df424c635c4a6a6f8a0c2061dc59068c283d28
lavanyakumareshbabu <lavanyakb39@gmail.com>
2016-10-19 15:55:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,11 +74,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 }); 
 var names=[];
-app.get('/submit-name/:name', function (req, res) {
-    var name=req.params.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
+
 app.get('/counter', function (req, res) {
     counter=counter+1;
   res.send(counter,tostring());
@@ -94,7 +90,11 @@ app.get('/:articlename', function (req, res) {
     var articlename=req.params.articlename;
   res.send(createtemplate(articles[articlename]));
 });
-
+app.get('/submit-name/:name', function (req, res) {
+    var name=req.params.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
  
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
