ab9b207efe307a91494115e518b2b92deeb3a5c4
lavanyakumareshbabu <lavanyakb39@gmail.com>
2016-10-22 12:09:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,10 +73,10 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var c=0;
 app.get('/counter', function (req, res) {
-    counter=counter+1;
-  res.send(counter.toString());
+    c=c+1;
+  res.send(c.toString());
 }); 
 
 var names=[];
