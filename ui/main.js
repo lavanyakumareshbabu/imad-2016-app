6c1faecd99c4ff5f5d33ed2a357c47c03d5dd0bb
lavanyakumareshbabu <lavanyakb39@gmail.com>
2016-10-22 12:37:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,9 +14,9 @@ button.onclick=function()
         {
             if(request.status==200)
                {
-                   var counter=request.responseText;
+                   var c=request.responseText;
                    var span=document.getElementById('count');
-                   span.InnerHTML=counter.toString();
+                   span.InnerHTML=c.toString();
                    
                }
             
