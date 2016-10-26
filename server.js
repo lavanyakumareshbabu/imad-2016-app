115652bebaa2443f72bf7184393557cd1782e5f9
lavanyakumareshbabu <lavanyakb39@gmail.com>
2016-10-26 16:29:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var Pool=require('pg').Pool;
+var Pool = require('pg').Pool;
 var config={
        user:'lavanyakumareshbabu',
        database:'lavanyakumareshbabu',
