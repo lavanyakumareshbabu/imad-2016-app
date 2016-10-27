2b99206daf353d1788aa570a238adc3ee50c9255
lavanyakumareshbabu <lavanyakb39@gmail.com>
2016-10-27 10:28:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/test-db', function(req, res){
     pool.query('SELECT * FROM test',function(err,result){
      if (err)
      {
-          res.status(500).send(err,tostring());
+         res.status(500).send(err.toString());
      }
      else
      {
