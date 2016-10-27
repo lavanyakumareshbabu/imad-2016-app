d8fe47ecaf96b3b46bb2e1d1e40b24f08fe92dcc
lavanyakumareshbabu <lavanyakb39@gmail.com>
2016-10-27 13:47:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,10 +113,24 @@ app.get('/submit-name', function (req, res) {
 
 
 
-app.get('/:articlename', function (req, res) {
-    var articlename=req.params.articlename;
-  res.send(createtemplate(articles[articlename]));
+app.get('/articles:articlename', function (req, res) {
+    pool.query("SELECT * FROM article WHERE title='"+req.params.articlename+"'",function(err,result){
+        if(err)
+        {
+            res.status(500).send(err.tostring());
+        }
+        if(result.rows.length===0)
+        {
+            res.status(404).send(err.tosrting());
+        }
+        else
+        {
+            var articledata=result.rows[0];
+            res.send(createtemplate(articledata));
+        }    
+    });
 });
+  
 
  
 app.get('/ui/main.js', function (req, res) {
