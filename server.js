94a9dadaadf9243c17252cd1038fe0dfe4811b88
lavanyakumareshbabu <lavanyakb39@gmail.com>
2016-10-27 13:29:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/', function (req, res) {
 });
 var pool = new Pool(config);
 app.get('/test-db', function(req, res){
-    pool.query('INSERT INTO test("id","name") VALUES(4,lavan)',function(err,result){
+    pool.query('SELECT * FROM test',function(err,result){
      if (err)
      {
          res.status(500).send(err.toString());
