49ea7d271fb66a7a606cbffc1b4117d7f5f1ae71
lavanyakumareshbabu <lavanyakb39@gmail.com>
2016-11-02 17:35:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,34 +1,26 @@
-var button=document.getElementById('counter');
-
-button.onclick=function()
+var nameinput=document.getelementbyid('name');
+var name=nameinput.value;
+var submit=document.getelementbyid('submit-btn');
+submit.onclick=function()
 {
-    
-     
-     var request=new XMLHttpRequest();
-
-    request.open('GET','http://lavanyakumareshbabu.imad.hasura-app.io/counter',true);
-     request.send(null);
-      request.onreadystatechange = function()
-      {
-        if(request.readyState==XMLHttpRequest.DONE)
-        {
-            if(request.status==200)
-               {
-                   var c=request.responseText;
-                   var span=document.getElementById('count');
-                   span.InnerHTML=c.toString();
-                   
-               }
-            
-        }
-    
-      };
-      
-     
-     
-  };
- 
+     var request=newXMLhttprequest();
+     request.onreadystatechange=function()
+     {
+         if(request.readystate==XMLhttprequest.DONE)
+         { 
+             if (request.status==200)
+             {
+                 var names=request.responsetext;
+                 names=JSON.parse(names);
+                 var list='';
+                 for(var i=0;i<names.length;i++)
+                 {
+                     list+='<li>'+names[i]+'<li>';
+                 }
+                 var ul=document.getelementbyid('namelist');
+                 ul.innerHTML=list;
+             }
          
-
-
-
+         }
+     };
+};     
